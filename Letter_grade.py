5bbe21ec759b9f18ddabfd4a86afdb1dce8aceb6
DaveMansCave <damasta2789@hotmail.com>
2025-02-14 18:27:14 -0500

    Add files via upload

---
+++ Letter_grade.py
@@ -0,0 +1,16 @@
+def Letter_Grade(grade):
+    if grade >= 90:
+     print('A+')
+    elif grade <= 89 and grade >= 85:
+        print('A')
+    elif grade <= 84 and grade >= 80:
+        print('A-')
+    elif grade <= 79 and grade >= 77:
+        print('B+')
+    elif grade <= 76 and grade >= 73:
+        print('B')
+    elif grade <= 72 and grade >= 70:
+        print('B-')
+    elif grade < 70:
+        print('F')
+
